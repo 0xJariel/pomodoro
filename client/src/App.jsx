ba195295840e7e35119045226fa723f8bbe9bece
J <0xjariel@protonmail.com>
2023-09-15 17:39:18 -0600

    user info retrieved successfully (playlists, user info, etc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,69 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { useLocation, Outlet } from "react-router-dom";
 import useAuth from "./components/useAuth";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "9cae559518c9414eaf7cb90673188a83",
+});
 
 function App() {
+  const [playlistID, setPlaylistID] = useState();
+  const [playlists, setPlaylists] = useState();
+  const [displayName, setDisplayName] = useState();
   const location = useLocation();
 
   const code = useMemo(() => {
     return new URLSearchParams(location.search).get("code");
   }, [location.search]);
 
-  const AuthToken = useAuth(code);
+  const accessToken = useAuth(code);
+
+  useEffect(() => {
+    if (!accessToken) return;
+    // set access token, playlists to choose from
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!accessToken) return;
+
+    // Get the authenticated user
+    spotifyApi.getMe().then(
+      function (data) {
+        console.log("Some information about the authenticated user", data.body);
+        setDisplayName(data.body.display_name);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+
+    spotifyApi.getPlaylist("5ieJqeLJjjI8iJWaxeBLuK").then(
+      function (data) {
+        console.log("Some information about this playlist", data.body);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+
+    // Get a user's playlists
+    spotifyApi.getUserPlaylists("thelinmichael").then(
+      function (data) {
+        console.log("Retrieved playlists", data.body);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+  }, [accessToken, playlistID]);
 
   return (
     <StyledApp>
       <Navbar />
       <Outlet />
-      {code ? <h4>{AuthToken}</h4> : <Footer />}
+      {code ? <h4>{accessToken}</h4> : <Footer />}
     </StyledApp>
   );
 }
